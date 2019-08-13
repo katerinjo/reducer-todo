7f5b49b26c1ed86f227f9a9ee96e8dbd30d0acad
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-08-13 13:31:03 -0700

    reducer scaffold

---
+++ mumpsimus/src/reducers/itemReducer.js
@@ -0,0 +1,13 @@
+export const initState = {
+  list: [
+    {
+      item: 'Learn about reducers',
+      completed: false,
+      id: 3892987589
+    }
+  ]
+};
+
+export const reduce = (state, action) => {
+  return state;
+};
